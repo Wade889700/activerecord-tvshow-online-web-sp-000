4230225d61127c48687556bc187035f094202a2f
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-24 22:43:59 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -1,5 +1,6 @@
-class AddSeasonToShows < ActiveRecord::Migration 
-  def change 
+class AddSeasonToShows < ActiveRecord::Migration
+  def change
+    add_column :season
+    add_column :string
   end
-
 end
